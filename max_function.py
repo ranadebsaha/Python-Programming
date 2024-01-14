35f7387f07b766f3f6ef053ddc7e6c22c9c11bea
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-01-14 10:55:24 +0530

    Create max_function.py

---
+++ max_function.py
@@ -0,0 +1,21 @@
+def max(a,b,c):
+
+    if(b<a>c):
+
+        return a
+
+    elif(a<b>c):
+
+        return b
+
+    else:
+
+        return c
+
+
+
+# print("Enter three number: ",end='')
+
+# a,b,c=[int(x) for x in input().split()]
+
+# print("Maximum number is: ",max(a,b,c))
