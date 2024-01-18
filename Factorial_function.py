feba7a74a27f3cd567b7b96c016648cab6cb618c
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-01-18 21:40:57 +0530

    Create Factorial_function.py

---
+++ Factorial_function.py
@@ -0,0 +1,15 @@
+def fact(a):
+
+    b=1
+
+    for i in range(1,a+1):
+
+        b=b*i
+
+    return b
+
+
+
+# a=int(input("Enter a Number: "))
+
+# print("The factorial of this number is: ",fact(a))
