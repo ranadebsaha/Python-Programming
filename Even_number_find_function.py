9094c44b2462daf4f05c1894d4bbd7bf93fb24eb
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-01-20 00:14:10 +0530

    Create Even_number_find_function.py

---
+++ Even_number_find_function.py
@@ -0,0 +1,17 @@
+def even(list):
+
+    for i in list:
+
+        if(i%2==0):
+
+           print(i,end=" ")
+
+
+
+
+
+# list1=[5,61,28,76,34,97,84,47]
+
+# print("Even numbers are: ")
+
+# even(list1)
