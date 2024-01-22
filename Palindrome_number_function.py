fbfd02f8375e1999a628fdaba5d63a74449b6b95
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-01-22 23:29:32 +0530

    Create Palindrome_number_function.py

---
+++ Palindrome_number_function.py
@@ -0,0 +1,27 @@
+def palindrome(a):
+
+    r=0
+
+    e=a
+
+    while(a!=0):
+
+        b=a%10
+
+        r=r*10+b
+
+        a=a//10
+
+    if(e==r):
+
+        print("This is a Palindrome Number.")
+
+    else:
+
+        print("This is not a Palindrome Number.")
+
+
+
+# a=int(input("Enter a number: "))
+
+# palindrome(a)  
