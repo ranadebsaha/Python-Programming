808dec5dc3242ca55c352ff67a186cdd4a58271d
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-01-23 12:42:25 +0530

    Create Fibonacci_series.py

---
+++ Fibonacci_series.py
@@ -0,0 +1,23 @@
+def fibo(n):
+
+    a=0
+
+    b=1
+
+    for i in range(0,n):       
+
+        print(a)
+
+        c=a+b
+
+        a=b
+
+        b=c
+
+        
+
+
+
+# n=int(input("Enter how many seris you want: "))
+
+# fibo(n)
